2b7f1caa9fd4a5f9caa6c9ba10c0eb16d236e97c
Marium Sultan <mas1300@nyu.edu>
2019-05-15 10:49:08 -0400

    trying slider

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,15 @@ var map = new mapboxgl.Map({
   zoom: 6.5
 });
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
+
 map.addSource('guns', {
   type: 'geojson',
   data: './data/cl.geojson',
