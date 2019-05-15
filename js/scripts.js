a0452adf8d8094cd0a6e064a1e91c6dac9ee1904
Marium Sultan <mas1300@nyu.edu>
2019-05-15 16:18:31 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,17 @@ var years = [
   '2018',
 ];
 
+map.addSource('guns_by_zip', {
+  type: 'geojson',
+  data: './data/simple_guns.geojson',
+});
+
+
+map.addSource('mass_shootings', {
+  type: 'geojson',
+  data: './data/mass_shootings.geojson',
+});
+
 function filterBy(year) {
 
   var filters = ['==', 'year', year];
