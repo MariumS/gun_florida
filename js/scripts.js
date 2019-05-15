f7adc163f57ccd95f9488ee52e84b11e12056b7c
Marium Sultan <mas1300@nyu.edu>
2019-05-15 11:29:28 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFyemlwYW45NCIsImEiOiJjanVrOTdwaDQxdG42NDRwN
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+//output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-};
+//slider.oninput = function() {
+  //output.innerHTML = this.value;
+//};
 
 var map = new mapboxgl.Map({
   container: 'mapContainer',
