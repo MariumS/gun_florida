5980e8491d3580211fd55df27d20aa72a3e19c6a
Marium Sultan <mas1300@nyu.edu>
2019-05-15 10:59:51 -0400

    adding popup

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,7 @@
 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFyemlwYW45NCIsImEiOiJjanVrOTdwaDQxdG42NDRwNGFmbzY5dWdtIn0.4lVQxPc89QYzHas2IIWmew';
 
-var map = new mapboxgl.Map({
-  container: 'mapContainer',
-  style: 'mapbox://styles/mapbox/outdoors-v11',
-  center: [-73.985130, 40.758896],
-  zoom: 6.5
-});
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFyemlwYW45NCIsImEiOiJjanVrOTdwaDQxdG42NDRwNGFmbzY5dWdtIn0.4lVQxPc89QYzHas2IIWmew';
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
@@ -18,19 +12,40 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 };
 
-map.addSource('guns', {
+var map = new mapboxgl.Map({
+  container: 'mapContainer',
+  style: 'mapbox://styles/mapbox/outdoors-v11',
+  center: [-73.985130, 40.758896],
+  zoom: 6.5
+});
+
+map.addSource('guns_by_zip', {
   type: 'geojson',
   data: './data/cl.geojson',
 });
 
 
+map.addSource('mass_shootings', {
+  type: 'geojson',
+  data: './data/mass_shootings.geojson',
+});
+
 map.on('load', function(){
+
+  new mapboxgl.Marker({
+    color: 'blue',
+  })
+    .setLngLat([mass_shootings.longitude, mass_shootings.latitude])
+    .setPopup(new mapboxgl.Popup({ offset: 10 })
+    .setText(`In ${mass_shootings.address} year ${mass_shootings.year}`))
+    .addTo(map);
+  });
 
 
       map.addLayer({
         id: 'guns_',
         type: 'fill',
-        source: 'guns',
+        source: 'guns_by_zip',
         paint: {
           'fill-color': [
             // use a curve (http://bl.ocks.org/anandthakker/raw/6d0269825a7e0381cdcde13f84a0b6b0/#types-expression-curve)
@@ -44,5 +59,5 @@ map.on('load', function(){
           'fill-opacity': 0.6
         },
         layout: {}
-      });
+  
       });
